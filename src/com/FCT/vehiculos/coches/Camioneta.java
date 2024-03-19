824865678be90c13456b7538a6d225f45542cdeb
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2024-03-19 15:10:31 +0000

    fix:Fixed methods

--- src/com/FCT/vehiculos/coches/Camioneta.java
+++ src/com/FCT/vehiculos/coches/Camioneta.java
@@ -1,5 +1,16 @@
 package com.FCT.vehiculos.coches;
 
-public class Camioneta {
+import com.FCT.vehiculos.motor.Motor;
 
+public class Camioneta {
+	private Motor motor;
+	
+	public Camioneta(Motor motor) {
+		this.motor = motor;
+	}
+	
+	public void arrancar(){
+		motor.encender();
+		System.out.println("Camioneta encendida.");
+	}
 }
